3d5770808e20f1e0de1cb74e6664431b4adf9863
Filip <fillip.detterfelt@gmail.com>
2024-09-16 12:13:42 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Devopsweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
