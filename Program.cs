f3dde2c44c5e4b4643dd0c3a1712e972d3408e5f
Filip <fillip.detterfelt@gmail.com>
2024-09-25 14:28:40 +0200

    small

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Devopsweb.Data;
 using DotNetEnv;
+using Microsoft.AspNetCore.Components.Sections;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
@@ -7,25 +8,59 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 Env.Load();
+string testenv = Environment.GetEnvironmentVariable("DB_HOST");
+Console.WriteLine("Testenv = " + testenv);
+string connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"] ?? string.Empty;
+Console.WriteLine("Connectionstring = " + connectionString);
+
+
+// Replace placeholders with actual environment variables
+connectionString = connectionString
+    .Replace("${DB_HOST}", Environment.GetEnvironmentVariable("DB_HOST") ?? string.Empty)
+    .Replace("${DB_DATABASE}", Environment.GetEnvironmentVariable("DB_DATABASE") ?? string.Empty)
+    .Replace("${DB_USERNAME}", Environment.GetEnvironmentVariable("DB_USERNAME") ?? string.Empty)
+    .Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD") ?? string.Empty);
+Console.WriteLine(connectionString);
+builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
+//Console.WriteLine(builder.Configuration["ConnectionStrings:DefaultConnection"]);
+
+Console.WriteLine(Environment.GetEnvironmentVariable("DB_HOST"));
+Console.WriteLine(Environment.GetEnvironmentVariable("DB_DATABASE"));
+Console.WriteLine(Environment.GetEnvironmentVariable("DB_USERNAME"));
+Console.WriteLine(Environment.GetEnvironmentVariable("DB_PASSWORD"));
+
+
+
+string saveStringFromBuilder = builder.Configuration["ConnectionStrings:DefaultConnection"];
+Console.WriteLine(connectionString);
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddDbContext<SkillsDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-    
+    options.UseNpgsql(connectionString));
+
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseNpgsql(saveStringFromBuilder));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+
+
 var app = builder.Build();
+Console.WriteLine("Connectionstring after replace = " + builder.Configuration["ConnectionStrings:DefaultConnection"]);
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).AddEnvironmentVariables();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
