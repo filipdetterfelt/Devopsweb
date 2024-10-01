639b05d728ac54ea2cac031d67fb9334bb10f0bc
Filip <fillip.detterfelt@gmail.com>
2024-10-01 14:36:10 +0200

    test

---
+++ DevopswebTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Devopsweb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devopsweb.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void HomeControllerTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
