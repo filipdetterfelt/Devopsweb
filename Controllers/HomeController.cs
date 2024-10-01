28bf6c380526e770d6753fdc05aa6e3515d84598
Filip <fillip.detterfelt@gmail.com>
2024-10-01 10:56:02 +0200

    test2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -35,7 +35,6 @@ namespace Devopsweb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
     }
 
    
